bff42d8de674c01e016a098f41fe75427cf37177
mh <mh@tran-huu.de>
2017-06-24 17:27:01 +0200

    Debugging

--- routes/calculate.js
+++ routes/calculate.js
@@ -41,50 +41,49 @@ module.exports = function (param) {
         if (duration > 84) {
             out = 'Viel zu lang, so lange wollen wir nicht auf das Geld warten. Maximal 84 Monate.'
         }
+    }
+    if (amount < 2500) {
+        if (out == '') {
+            out = 'So wenig? Bei weniger als 2500 Ocken bieten unsere Konten Dir ein tolles Dispo an. '
+        }
+        else {
+            out = out + 'Und ausserdem: Bei so einer kleinen Summe, kannst Du Deine Omi fragen'
+        }
+    }
 
-        if (amount < 2500) {
-            if (out == '') {
-                out = 'So wenig? Bei weniger als 2500 Ocken bieten unsere Konten Dir ein tolles Dispo an. '
+    if (amount > 50000) {
+
+        if (out == '') {
+            out = 'Boah, das ist ganz schön viel Geld, kontaktier uns über einen Oldschoolweg. '
+        }
+        else {
+            out = out + 'Und ausserdem: Bei so einer großen Summe, schau mal bei uns vorbei.'
+        }
+    }
+
+    if (out != '') {
+        out = out + ' PS: '
+    }
+
+    if (income < 1000) {
+        out = out + 'Du brauchst mindestens 1000 Euro, um bei uns einen Kredit zu bekommen.'
+    } else {
+        if (monthly > income) {
+            out = out + 'Wenn Du nicht mehr bei Mama und Papa wohnst, würden wir Dir nicht empfehlen das Angebot anzunehmen.'
+        } else {
+            if (monthly > income - 1000) {
+                out = out + 'Wenn Du mehr essen willst, als Brot und Wasser, würden wir Dir empfehlen die Raten zu verringern.'
             }
             else {
-                out = out + 'Und ausserdem: Bei so einer kleinen Summe, kannst Du Deine Omi fragen'
-            }
-        else
-            {
-                if (amount > 50000) {
+                out = 'Wir haben ein tolles Angebot für Dich mit nur ' + nominalInterest + '% Zinsen ('
+                    + effectiveInterest + '%effektiv Zins). Du zahlst ' + duration + ' monatliche Raten a ' + monthly +
+                    ' EUR.'
 
-                    if (out == '') {
-                        out = 'Boah, das ist ganz schön viel Geld, kontaktier uns über einen Oldschoolweg. '
-                    }
-                    else {
-                        out = out + 'Und ausserdem: Bei so einer großen Summe, schau mal bei uns vorbei.'
-                    }
-                }
-
-                if (out != '') {
-                    out = out + ' PS: '
-                }
-
-                if (income < 1000) {
-                    out = out + 'Du brauchst mindestens 1000 Euro, um bei uns einen Kredit zu bekommen.'
-                } else {
-                    if (monthly > income) {
-                        out = out + 'Wenn Du nicht mehr bei Mama und Papa wohnst, würden wir Dir nicht empfehlen das Angebot anzunehmen.'
-                    } else {
-                        if (monthly > income - 1000) {
-                            out = out + 'Wenn Du mehr essen willst, als Brot und Wasser, würden wir Dir empfehlen die Raten zu verringern.'
-                        }
-                        else {
-                            out = 'Wir haben ein tolles Angebot für Dich mit nur ' + nominalInterest + '% Zinsen ('
-                                + effectiveInterest + '%effektiv Zins). Du zahlst ' + duration + ' monatliche Raten a ' + monthly +
-                                ' EUR.'
-
-                        }
-                    }
-                }
             }
         }
-        return {
-            speech: out,
-            data: {}
-        }
+    }
+}
+return {
+    speech: out,
+    data: {}
+}
