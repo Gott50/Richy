06ad13452d9c63097cc80cb1e639ad1f6e315d6a
Gott50 <xxGott50xx@gmail.com>
2017-06-24 12:13:53 +0200

    add POST /hook

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/hook', function (req, res) {
+    console.log(req)
+});
+
 module.exports = router;
